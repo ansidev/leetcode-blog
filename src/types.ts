ac9efe9e08076edee2760a0c4c4dfadf24443cd2
Le Minh Tri <ansidev@gmail.com>
2022-11-10 02:55:28 +0700

    fix: correct typescript config

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ export declare interface PostFrontMatter {
   layout?: string
   title: string
   slug: string
-  keywords?: string
+  keywords?: string[]
   author: string
   pubDate: string
   tags?: string[]
