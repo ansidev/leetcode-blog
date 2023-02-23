c6ee40931ee89a1ff4eadf8b104b4254ea22ed4c
Le Minh Tri <ansidev@gmail.com>
2023-02-24 02:58:59 +0700

    feat: update index page UI using TailwindCSS

---
+++ src/configs/site.ts
@@ -0,0 +1,7 @@
+export default {
+  title: 'LeetCode Blog',
+  description: 'Solutions for LeetCode problems - Written by ansidev',
+  author: 'ansidev',
+  favicon: '/favicon.ico',
+  faviconMimeType: 'image/ico',
+}
